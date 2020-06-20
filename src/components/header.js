7b0d31c795c79beaff622c67e3a8e9cd61fcd875
Hassan Mahmud <hassan.mahmud@lego.com>
2020-06-20 20:20:29 +0200

    Refactor header to show full screen menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,62 @@
 import { graphql, useStaticQuery, Link } from "gatsby";
+import PropTypes from "prop-types";
 import React, { useState } from "react";
+//import Modal from './modal';
+
+import MenuIcon from "../images/menu.inline.svg";
+import CloseIcon from "../images/close.inline.svg";
+
+const navLinks = [
+  {
+    route: `/about`,
+    title: `About`,
+  },
+  {
+    route: `/contact`,
+    title: `Contact`,
+  },
+];
+
+function MobileFullScreenNavMenu({ onClose }) {
+
+  const handleClose = (e) => {
+    e.preventDefault();
+    onClose()
+  };
+
+  return (
+    <nav className="md:hidden fixed inset-0 z-50 bg-steel-blue flex justify-center">
+
+      <buton className="absolute top-0 bottom-0 right-0 px-6 py-5 text-white"
+        type="button"
+        aria-label="Close menu" aria-controls="navigation">
+        <CloseIcon title="close"
+          className="w-5 h-5 font-thin fill-current"
+          onClick={handleClose} />
+      </buton>
+
+      <ul className="flex-row items-center p-6 text-xl text-center">
+        {navLinks.map((link, index) => (
+          <li className="my-3" key={index}>
+            <Link
+              className="text-white no-underline"
+              key={link.title}
+              to={link.route}
+            >
+              {link.title}
+            </Link>
+          </li>))}
+      </ul>
+    </nav>
+  );
+}
+
+MobileFullScreenNavMenu.propTypes = {
+  onClose: PropTypes.func.isRequired
+}
 
 function Header() {
-  const [isExpanded, toggleExpansion] = useState(false);
+  const [isExpanded, setExpansion] = useState(false);
   const { site } = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -13,64 +67,44 @@ function Header() {
     }
   `);
 
+  const handleMenuClose = () => {
+    setExpansion(false);
+  }
+
   return (
-    <header className="bg-teal-700">
-      <div className="flex flex-wrap items-center justify-between max-w-4xl p-4 mx-auto md:p-8">
-        <Link to="/">
-          <h1 className="flex items-center text-white no-underline">
-            <svg
-              className="w-8 h-8 mr-2 fill-current"
-              height="54"
-              viewBox="0 0 54 54"
-              width="54"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-            </svg>
-            <span className="text-xl font-bold tracking-tight">
-              {site.siteMetadata.title}
-            </span>
-          </h1>
-        </Link>
-
-        <button
-          className="flex items-center block px-3 py-2 text-white border border-white rounded md:hidden"
-          onClick={() => toggleExpansion(!isExpanded)}
-        >
-          <svg
-            className="w-3 h-3 fill-current"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+    <header className="bg-steel-blue">
+      <div className="flex flex-wrap justify-between max-w-4xl mx-auto md:p-8">
+        <div className="bg-geraldine flex-grow p-4">
+          <Link to="/">
+            <h1 className="flex items-center text-white no-underline">
+              <span className="text-xl font-bold tracking-tight">
+                {site.siteMetadata.title}
+              </span>
+            </h1>
+          </Link>
+        </div>
+        <div className="m-4">
+          <button
+            className="px-2 py-1 text-white md:hidden"
+            aria-label="Menu" aria-controls="navigation"
+            onClick={() => setExpansion(true)}
           >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
+            <MenuIcon title="Menu" className="w-5 h-5 fill-current" />
+          </button>
+          {isExpanded ? <MobileFullScreenNavMenu onClose={handleMenuClose} /> : null}
 
-        <nav
-          className={`${
-            isExpanded ? `block` : `hidden`
-          } md:block md:flex md:items-center w-full md:w-auto`}
-        >
-          {[
-            {
-              route: `/about`,
-              title: `About`,
-            },
-            {
-              route: `/contact`,
-              title: `Contact`,
-            },
-          ].map((link) => (
-            <Link
-              className="block mt-4 text-white no-underline md:inline-block md:mt-0 md:ml-6"
-              key={link.title}
-              to={link.route}
-            >
-              {link.title}
-            </Link>
-          ))}
-        </nav>
+          <nav className="hidden md:block md:flex md:items-center w-full md:w-auto">
+            {navLinks.map((link) => (
+              <Link
+                className="block mt-4 text-white no-underline md:inline-block md:mt-0 md:ml-6"
+                key={link.title}
+                to={link.route}
+              >
+                {link.title}
+              </Link>
+            ))}
+          </nav>
+        </div>
       </div>
     </header>
   );
