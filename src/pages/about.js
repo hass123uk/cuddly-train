f2ee113061949aa0bc01957d61187c08ef91f421
Hassan Mahmud <hassan.mahmud@lego.com>
2020-06-21 23:03:09 +0200

    Work towards a finished about/my story page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import dogIllustration from "../images/dog-illustration.svg";
 import Container from "../components/container";
 
 function AboutPage() {
@@ -12,26 +11,32 @@ function AboutPage() {
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
         title="About"
       />
-      <Container>
-        <section className="flex flex-col items-center md:flex-row">
-          <div className="md:w-2/3 md:mr-8">
-            <blockquote className="pl-4 font-serif leading-loose text-justify border-l-4 border-gray-900">
-              The point is... to live one&apos;s life in the full complexity of
-              what one is, which is something much darker, more contradictory,
-              more of a maelstrom of impulses and passions, of cruelty, ecstacy,
-              and madness, than is apparent to the civilized being who glides on
-              the surface and fits smoothly into the world.
-          </blockquote>
-
-            <cite className="block mt-4 text-xs font-bold text-right uppercase">
-              – Thomas Nagel
-          </cite>
-          </div>
-
-          <figure className="w-2/3 md:w-1/3">
-            <img alt="A dog relaxing" src={dogIllustration} />
-          </figure>
-        </section>
+      <Container className="bg-accent text-white text-center">
+        <h1 className="text-4xl font-bold leading-loose">My story</h1>
+        <p className="leading-loose">
+          The point is... to live one&apos;s life in the full complexity of
+          what one is, which is something much darker, more contradictory,
+          more of a maelstrom of impulses and passions, of cruelty, ecstacy,
+          and madness, than is apparent to the civilized being who glides on
+          the surface and fits smoothly into the world.
+          </p>
+      </Container>
+      <Container className="bg-accent2">
+        <div className="border rounded border-main p-3">
+          <p className="text-center text-main text-2xl font-thin">
+            &quot;You either take control of your love life or it takes control of you. It’s that simple.&quot;
+          </p>
+        </div>
+      </Container>
+      <Container className="bg-accent2 text-center">
+        <h1 className="text-2xl text-main font-bold leading-loose">My Mission</h1>
+        <p className="leading-loose">
+          The point is... to live one&apos;s life in the full complexity of
+          what one is, which is something much darker, more contradictory,
+          more of a maelstrom of impulses and passions, of cruelty, ecstacy,
+          and madness, than is apparent to the civilized being who glides on
+          the surface and fits smoothly into the world.
+          </p>
       </Container>
     </Layout>
   );
