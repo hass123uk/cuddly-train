c2179025926e06934902f254e4de6eb849e6e9af
Hassan Mahmud <hassan.mahmud@lego.com>
2020-10-11 21:59:09 +0200

    Copy contact and my story into index.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,16 @@
 import React from "react";
+import { Link } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Container from "../components/container";
 import portraitPicture from "../images/portrait.jpg";
+import ContactForm from "../components/contact-form";
 
 function IndexPage() {
   return (
     <Layout>
-      <SEO
-        keywords={[`life coach`, `counseling`, `nlp coach`]}
-        title="Home"
-      />
+      <SEO keywords={[`life coach`, `counseling`, `nlp coach`]} title="Home" />
 
       <section>
         <img
@@ -24,40 +23,53 @@ function IndexPage() {
             Life Coach &amp; Certified NLP Practitioner
           </h1>
           <p className="text-md ">Helping you to get through it.</p>
-          <button
-            type="button"
-            className="bg-accent rounded-full py-2 px-4 my-5"
-            aria-label="Go to contact me page" aria-controls="navigation"
-          >
-            Work with me
-          </button>
+          <Link to="/contact">
+            <button
+              type="button"
+              className="bg-accent rounded-full py-2 px-4 my-5"
+              aria-label="Go to contact me page"
+              aria-controls="navigation"
+            >
+              Work with me
+            </button>
+          </Link>
         </Container>
       </section>
-
-      {/* <section className="text-center">
-        <img
-          alt="Cat and human sitting on a couch"
-          className="block w-1/2 mx-auto mb-8"
-          src={catAndHumanIllustration}
-        />
-
-        <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400">
-          Hey there! Welcome to your first Gatsby site.
-        </h2>
-
+      <Container className="bg-accent text-white text-center">
+        <h1 className="text-4xl font-bold leading-loose">My story</h1>
         <p className="leading-loose">
-          This is a barebones starter for Gatsby styled using{` `}
-          <a
-            className="font-bold text-gray-900 no-underline"
-            href="https://tailwindcss.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Tailwind CSS
-          </a>
-          , a utility-first CSS framework.
+          The point is... to live one&apos;s life in the full complexity of what
+          one is, which is something much darker, more contradictory, more of a
+          maelstrom of impulses and passions, of cruelty, ecstacy, and madness,
+          than is apparent to the civilized being who glides on the surface and
+          fits smoothly into the world.
         </p>
-      </section> */}
+      </Container>
+      <Container className="bg-accent2">
+        <div className="border rounded border-main p-3">
+          <p className="text-center text-main text-2xl font-thin">
+            &quot;You either take control of your love life or it takes control
+            of you. It’s that simple.&quot;
+          </p>
+        </div>
+      </Container>
+      <Container className="bg-accent2 text-center">
+        <h2 className="text-2xl text-main font-bold leading-loose">
+          My Mission
+        </h2>
+        <p className="leading-loose">
+          The point is... to live one&apos;s life in the full complexity of what
+          one is, which is something much darker, more contradictory, more of a
+          maelstrom of impulses and passions, of cruelty, ecstacy, and madness,
+          than is apparent to the civilized being who glides on the surface and
+          fits smoothly into the world.
+        </p>
+      </Container>
+      <Container className="bg-main text-white">
+        <section>
+          <ContactForm />
+        </section>
+      </Container>
     </Layout>
   );
 }
