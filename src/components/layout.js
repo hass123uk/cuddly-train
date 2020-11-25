515cbaa5e80819f8cff40d87ad32e643f54d081d
Hassan Mahmud <hassan.mahmud@lego.com>
2020-11-25 21:46:15 +0100

    Fix side space on desktop

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,12 +6,14 @@ import Footer from "./footer";
 
 function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen font-sans text-gray-900 bg-accent2 md:px-2">
-      <Header className="fixed top-0 left-0 right-0 z-10 md:min-w-980" />
+    <div className="flex flex-col min-h-screen font-sans text-gray-900 bg-accent2 md:px-14">
+
+      {/* these: top-0 left-0 md:mx-14 are needed because fixed does not play nice with flex */}
+      <Header className="fixed top-0 left-0 right-0 z-10 md:min-w-980 md:mx-14" />
 
       <main className="mt-20 flex-1 w-full md:min-w-980">{children}</main>
 
-      <Footer className="md:min-w-980"/>
+      <Footer className="md:min-w-980 md:mb-16"/>
     </div>
   );
 }
