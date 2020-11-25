eb5609f2a9746abda926a29f0dd461fab0d51f1e
Hassan Mahmud <hassan.mahmud@lego.com>
2020-11-25 15:22:03 +0100

    Add to tailwind custom minWidth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,21 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
-  purge: [
-    './src/**/*.js',
-  ],
+  purge: ["./src/**/*.js"],
   theme: {
     extend: {
       colors: {
-        'main': '#2C2E83',
-        'accent': '#EB6C6B',
-        'accent2': '#FEF0E4',
-        'accent3': '#FEF0E3',
-      }
-    }
+        main: "#2C2E83",
+        accent: "#EB6C6B",
+        accent2: "#FEF0E4",
+        //'accent3': '#FEF0E3',
+        accent3: "#000000",
+      },
+      minWidth: {
+        "980": "980px",
+      },
+    },
   },
   variants: {},
   // https://github.com/tailwindcss/custom-forms
-  plugins: [require('@tailwindcss/custom-forms')],
+  plugins: [require("@tailwindcss/custom-forms")],
 };
