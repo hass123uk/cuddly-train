6c067639131182b4ce70c83c781e4665a6b36623
Hassan Mahmud <hassan.mahmud@lego.com>
2020-11-25 16:22:25 +0100

    Update tailwind - install postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
   purge: ["./src/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
@@ -15,7 +16,9 @@ module.exports = {
       },
     },
   },
-  variants: {},
+  variants: {
+    extend: {},
+  },
   // https://github.com/tailwindcss/custom-forms
   plugins: [require("@tailwindcss/custom-forms")],
 };
