d533724ace3843ccb53fca83cfcc00f48a5933c4
Hassan Mahmud <hassan.mahmud@lego.com>
2020-11-26 16:34:11 +0100

    Style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,23 +1,29 @@
 import React from "react";
-
+import { Link } from "gatsby";
+import Container from "../components/container";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import abductionIllustration from "../images/abduction-illustration.svg";
 
 function NotFoundPage() {
   return (
     <Layout>
       <SEO title="404: Not found" />
-      <div>
-        <img
-          alt="Ghost getting abducted by aliens"
-          className="block mx-auto w-1/2"
-          src={abductionIllustration}
-        />
-        <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
-          Looks like this page is a ghost that got abducted by aliens...
-        </h2>
-      </div>
+      <Container className="bg-main text-white text-center lg:text-left lg:p-32 lg:flex-1">
+          <h1 className="text-4xl font-bold leading-loose lg:text-6xl lg:mb-4">Page not found</h1>
+          <p className="leading-loose">
+            An error occurred and the page you are looking for could not be found. 
+          </p>
+          <Link to="/">
+              <button
+                type="button"
+                className="bg-accent rounded-full py-2 px-4 my-5"
+                aria-label="Go to home page"
+                aria-controls="navigation"
+              >
+                Home
+              </button>
+            </Link>
+        </Container>
     </Layout>
   );
 }
